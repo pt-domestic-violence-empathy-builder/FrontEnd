fb200fccb066a55ca50e98a31946fb22b490c6f1
kiamco <kiamcoo@gmail.com>
2019-08-21 21:01:51 -0700

    initial file structure complted

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/index.css'
+
+function App() {
+  return (
+    <div className="App">
+      hello0ooooooooooooooooooooooo
+      
+    </div>
+  );
+}
+
+export default App;

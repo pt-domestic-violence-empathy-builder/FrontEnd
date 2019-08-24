0477905a7ab49789184f5cad44cb3cde432fe0bb
kiamco <kiamcoo@gmail.com>
2019-08-23 20:53:59 -0700

    fixed formik issue

--- calculator/src/components/Location/destination.js
+++ calculator/src/components/Location/destination.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {Formik, FormikProps, Form, Field, withFormik} from 'formik';
+import {Button} from 'semantic-ui-react';
 import Styled from 'styled-components';
 import * as Yup from 'yup';
 
-const DestinationForm = ({ errors, touched, values}) => {
+const DestinationForm = ({errors, touched, values, status}) => {
 
     const LocationBox = Styled.div `
         display:flex;
@@ -21,37 +22,65 @@ const DestinationForm = ({ errors, touched, values}) => {
         margin: 10px auto;
     `;
 
-    const [locations, SetLocations] = useState([])
+    const [locations,
+        setLocations] = useState([])
+
+    useEffect(() => {
+        if (status) {
+            setLocations([
+                ...locations,
+                status
+            ]);
+            console.log(status)
+        }
+
+    }, [status])
 
     return (
-        <Formik>
-            <Form>
-                <WindowForm>
-                    <LocationContainer>
-                        <LocationBox>
-                            <h4 className='location-label'>Location</h4>
-                            <Field type='text' className='location-input' name='cityLocation' placeholder='city'/>
-                            {touched.cityLocation && errors.cityLocation && (
-                                <p>{errors.cityLocation}</p>
-                            )}
-                            <Field type='text' className='location-input' name='zipLocation' placeholder='zip'/>
-                        </LocationBox>
-                        <LocationBox>
-                            <h4 className='destination-label'>Destination</h4>
-                            <Field type='text' className='location-input' name='cityDestination' placeholder='city'/>
-                            <Field type='text' className='location-input' name='zipDestination' placeholder='zip'/>
-                        </LocationBox>
-                    </LocationContainer>
-                </WindowForm>
-
-                <button>next</button>
-            </Form>
-        </Formik>
+        <Form>
+            <div className='moving-form'>
+                <div className='location-form'>
+                    <h4 className='location-label'>Location</h4>
+                    <Field
+                        type='text'
+                        className='location-input'
+                        name='cityLocation'
+                        placeholder='city'/> {touched.cityLocation && errors.cityLocation && (
+                        <p>{errors.cityLocation}</p>
+                    )}
+                    <Field
+                        type='text'
+                        className='location-input'
+                        name='zipLocation'
+                        placeholder='zip'/> {touched.zipLocation && errors.zipLocation && (
+                        <p>{errors.zipLocation}</p>
+                    )}
+                </div>
+                <div className='destination-form'>
+                    <h4 className='destination-label'>Destination</h4>
+                    <Field
+                        type='text'
+                        className='location-input'
+                        name='cityDestination'
+                        placeholder='city'/> {touched.cityDestination && errors.cityDestination && (
+                        <p>{errors.cityDestination}</p>
+                    )}
+                    <Field
+                        type='text'
+                        className='location-input'
+                        name='zipDestination'
+                        placeholder='zip'/> {touched.zipDestination && errors.zipDestination && (
+                        <p>{errors.zipDestination}</p>
+                    )}
+                </div>
+                <button type='submit'>Next</button>
+            </div>
+        </Form>
     )
 }
 
 const formikHOC = withFormik({
-    mapPropsToValues({cityLocation, zipLocation, cityDestination, zipDestination}){
+    mapPropsToValues({cityLocation, zipLocation, cityDestination, zipDestination}) {
         return {
             cityLocation: cityLocation || '',
             zipLocation: zipLocation || '',
@@ -59,14 +88,26 @@ const formikHOC = withFormik({
             zipDestination: zipDestination || ''
         };
     },
-    validationSchema: Yup.object().shape({
-        cityLocation: Yup.string().required("input required"),
-        zipLocation: Yup.number().required("input required"),
-        cityDestination: Yup.string().required("input required"),
-        zipDestination: Yup.number().required("input required"),
-    }),
-    handleSubmit(values, {setStatus, resetForm}){
-        console.log(values);
+    validationSchema: Yup
+        .object()
+        .shape({
+            cityLocation: Yup
+                .string()
+                .required("input required"),
+            zipLocation: Yup
+                .number()
+                .required("needs to a zip code"),
+            cityDestination: Yup
+                .string()
+                .required("input required"),
+            zipDestination: Yup
+                .number()
+                .required("needs to a zip code")
+        }),
+    handleSubmit(values, {setStatus, resetForm}) {
+        console.log("handleSubmit: then: res: ", values);
+        setStatus(values);
+        resetForm();
     }
 })(DestinationForm)
 

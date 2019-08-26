78f086b9c63263258b0ce05f6284ab991ca8c518
kiamco <kiamcoo@gmail.com>
2019-08-25 22:59:18 -0700

    finished transportation options

--- calculator/src/components/Location/transportation.js
+++ calculator/src/components/Location/transportation.js
@@ -1,51 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Formik, FormikProps, Form, Field, withFormik } from 'formik';
-import { Button } from 'semantic-ui-react';
+import React, {useEffect, useState} from 'react';
+import {Formik, FormikProps, Form, Field, withFormik} from 'formik';
+import {Button} from 'semantic-ui-react';
 import Styled from 'styled-components';
 import * as Yup from 'yup';
 import Axios from 'axios';
 
 const Transportation = (props) => {
-    const {
-        touched,
-        values,
-        errors,
-        status
-    } = props;
+    const {touched, values, errors, status} = props;
     const [transitData,
         setTransitData] = useState([]);
 
+    const [show,
+        setShow] = useState(true);
+
     useEffect(() => {
         if (status) {
-            setTransitData([...transitData,status]);
+            setTransitData([
+                ...transitData,
+                status
+            ]);
         }
+
     }, [status])
 
     return (
         <div>
-            <h4>Method of transportation</h4>
-            <Field component='select' name='movingOptions' placeholder='Moving Options'>
-                <option> Select Transportation</option>
-                <option value={true}>Car</option>
-                <option value={true}>Moving Truck</option>
-            </Field>
-            <Form>
-                
-                <button type='submit'>next</button>
+            <Form className='transit-options'>
+                <h4>Method of Transportation</h4>
+                <label >
+                    <span class="checkmark"></span>
+                    <Field type="checkbox" name="car" checked={values.car}/>
+                    Car
+                </label>
+                <label >
+                    <span class="checkmark"></span>
+                    <Field type="checkbox" name="truck" checked={values.truck} />
+                    Truck
+                </label>
+                <label >
+                    <span class="checkmark"></span>
+                    <Field type="checkbox" name="other" checked={values.other} />
+                    Other
+                </label>
+                <button type='submit'>Next</button>
             </Form>
         </div>
     )
 }
 
 const formikHOC = withFormik({
-    mapToPropsValues({isCar, isTruck}) {
+    mapToPropsValues({car, truck, other}) {
         return {
-            isCar: isCar || false,
-            isTruck: isTruck || false
+            car: car || false,
+            truck: truck || false,
+            other: other || false
         };
-    }, 
-    handleSubmit(values,{setStatus, resetForm}){
-        console.log('handleSubmit: then: res ', values );
+    },
+    handleSubmit(values, {setStatus, resetForm}) {
+        console.log('handleSubmit: then: res ', values);
         setStatus(values);
         resetForm();
     }

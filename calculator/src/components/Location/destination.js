8e689691fa1298cd51b048dd373c328631d6dd80
kiamco <kiamcoo@gmail.com>
2019-08-26 23:12:46 -0700

    took out tests sections

--- calculator/src/components/Location/destination.js
+++ calculator/src/components/Location/destination.js
@@ -1,16 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {Formik, FormikProps, Form, Field, withFormik} from 'formik';
-import {Button} from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Formik, FormikProps, Form, Field, withFormik } from 'formik';
+import { Button } from 'semantic-ui-react';
 import Styled from 'styled-components';
 import * as Yup from 'yup';
 import Axios from 'axios';
 
-const DestinationForm = ({errors, touched, values, status, onChange, history}) => {
+const DestinationForm = ({ errors, touched, values, status, onChange, history }) => {
     const [locations,
-        setLocations] = useState([])
+        setLocations
+    ] = useState([])
 
     const [distance,
-        setDistance] = useState(0)
+        setDistance
+    ] = useState(0)
 
     useEffect(() => {
         if (status) {
@@ -32,58 +34,66 @@ const DestinationForm = ({errors, touched, values, status, onChange, history}) =
 
             history.push('/transportation')
         }
-        
+
 
     }, [status])
 
-    return (
-        <div>
-            <Form>
-                <div className='moving-form'>
-                    <div className='location-form'>
-                        <h4 className='location-label'>Location</h4>
-                        <Field
-                            type='text'
-                            className='location-input'
-                            name='cityLocation'
-                            placeholder='city, state'/> {touched.cityLocation && errors.cityLocation && (
-                            <p>{errors.cityLocation}</p>
-                        )}
-                        <Field
-                            type='text'
-                            className='location-input'
-                            name='zipLocation'
-                            placeholder='zip'/> {touched.zipLocation && errors.zipLocation && (
-                            <p>{errors.zipLocation}</p>
-                        )}
-                    </div>
-                    <div className='destination-form'>
-                        <h4 className='destination-label'>Destination</h4>
-                        <Field
-                            type='text'
-                            className='location-input'
-                            name='cityDestination'
-                            placeholder='city, state'/> {touched.cityDestination && errors.cityDestination && (
-                            <p>{errors.cityDestination}</p>
-                        )}
-                        <Field
-                            type='text'
-                            className='location-input'
-                            name='zipDestination'
-                            placeholder='zip'/> {touched.zipDestination && errors.zipDestination && (
-                            <p>{errors.zipDestination}</p>
-                        )}
-                    </div>
-                    <button type='submit'>Next</button>
-                </div>
-            </Form>
-            total distance: {distance}
-        </div>
+    return ( <
+        div >
+        <
+        Form >
+        <
+        div className = 'moving-form' >
+        <
+        div className = 'location-form' >
+        <
+        h4 className = 'location-label' > Location < /h4> <
+        Field type = 'text'
+        className = 'location-input'
+        name = 'cityLocation'
+        placeholder = 'city, state' / > {
+            touched.cityLocation && errors.cityLocation && ( <
+                p > { errors.cityLocation } < /p>
+            )
+        } <
+        Field type = 'text'
+        className = 'location-input'
+        name = 'zipLocation'
+        placeholder = 'zip' / > {
+            touched.zipLocation && errors.zipLocation && ( <
+                p > { errors.zipLocation } < /p>
+            )
+        } <
+        /div> <
+        div className = 'destination-form' >
+        <
+        h4 className = 'destination-label' > Destination < /h4> <
+        Field type = 'text'
+        className = 'location-input'
+        name = 'cityDestination'
+        placeholder = 'city, state' / > {
+            touched.cityDestination && errors.cityDestination && ( <
+                p > { errors.cityDestination } < /p>
+            )
+        } <
+        Field type = 'text'
+        className = 'location-input'
+        name = 'zipDestination'
+        placeholder = 'zip' / > {
+            touched.zipDestination && errors.zipDestination && ( <
+                p > { errors.zipDestination } < /p>
+            )
+        } <
+        /div> <
+        button type = 'submit' > Next < /button> <
+        /div> <
+        /Form> <
+        /div>
     )
 }
 
 const formikHOC = withFormik({
-    mapPropsToValues({cityLocation, zipLocation, cityDestination, zipDestination}) {
+    mapPropsToValues({ cityLocation, zipLocation, cityDestination, zipDestination }) {
         return {
             cityLocation: cityLocation || '',
             zipLocation: zipLocation || '',
@@ -99,15 +109,15 @@ const formikHOC = withFormik({
                 .required("input required"),
             zipLocation: Yup
                 .number(),
-                
+
             cityDestination: Yup
                 .string()
                 .required("input required"),
             zipDestination: Yup
                 .number()
-                
+
         }),
-    handleSubmit(values, {setStatus, resetForm}) {
+    handleSubmit(values, { setStatus, resetForm }) {
         console.log("handleSubmit: then: res: ", values);
         setStatus(values);
         resetForm();

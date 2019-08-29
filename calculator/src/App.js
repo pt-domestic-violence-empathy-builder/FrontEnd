7a448571d0cb91b047cd363d5c2411e70290cb94
kiamco <kiamcoo@gmail.com>
2019-08-28 17:26:59 -0700

    add budget form

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './css/index.css';
 import Location from './components/Location/LocationRouter.js';
 import Health from './components/Health/HealthRouter.js'
+import Budget from './components/Budget/budgetRouter.js'
 
 function App() {
   return (
     <div className="App">
       <Location />
       <Health />
-      
+      <Budget />
     </div>
   );
 }
---
+++ calculator/src/components/Budget/userBudget.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import {Formik, FormikProps, Form, Field, withFormik} from 'formik';
+
+import * as Yup from 'yup';
+
+const UserBudget = (props) => {
+    const {
+        errors,
+        touched,
+        values,
+        status,
+        onChange,
+        history
+    } = props
+
+    const [budget,
+        setBudget] = useState([])
+
+    useEffect(() => {
+        if (status) {
+            setBudget([
+                ...budget,
+                status
+            ])
+            onChange(status);
+        }
+
+    }, [status])
+
+    return (
+        <div className='budget-form'>
+            <Form>
+                <h4>Let's get started.What is your budget?</h4>
+                <Field type='input' className='budget-input' name='budget' placeholder='$'/>
+                <button type='submit'>Submit</button>
+            </Form>
+        </div>
+    )
+}
+
+const formikHOC = withFormik({
+    mapToPropsValues({budget}) {
+        return {
+            budget: budget || ''
+        };
+    },
+    handleSubmit(values, {setStatus, resetForm}) {
+        console.log('handle submit: budget:', values);
+        setStatus(values);
+    }
+})(UserBudget)
+
+export default formikHOC;

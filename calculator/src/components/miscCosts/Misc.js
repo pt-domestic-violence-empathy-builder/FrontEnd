8f2b9f96fad91f6c841f66108f297e32528d3ad2
Amber Sorensen <ambersorensen@Ambers-MBP.hsd1.ut.comcast.net>
2019-08-29 16:40:35 -0600

    finished the Misc Costs component, moving to router codes

--- calculator/src/components/miscCosts/Misc.js
+++ calculator/src/components/miscCosts/Misc.js
@@ -10,4 +10,52 @@ function MiscCosts(props){
         onChange,
         history
     }=props
+
+    const [miscCost, setMiscCost]=useState([]);
+
+    useEffect(()=>{
+        if(status){
+            setMiscCost([...miscCost,status]);
+        }
+        onChange(status);
+    },[status])
+
+    return(
+        <div>
+            <Form>
+                <Field
+                    type='text'
+                    name='legalCosts'
+                    placeholder='Any expected Legal Costs'
+                    onChange={miscClickHandler}/>
+                    
+                <Field
+                    type='text'
+                    name='securityCosts'
+                    placeholder='Any additional security measures'
+                    onChange={miscClickHandler}/>
+                <Field
+                    type='text'
+                    name='cellPhone'
+                    placeholder='Cost of a new Phone and/or number'
+                    onChange={miscClickHandler}/>
+
+                <button>Calculate Miscellaneous Costs</button>
+            </Form>
+        </div>
+    )
 }
+
+const formikHOC = withFormik({
+    mapToPropsValues({legalCosts,securityCosts,cellPhone}) {
+
+        return {
+            legalCosts:legalCosts||'',
+            securityCosts:securityCosts||'',
+            cellPhone:cellPhone||''
+
+        };
+    }
+})(MiscCosts)
+
+export default formikHOC;
